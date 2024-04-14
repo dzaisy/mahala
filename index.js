9d5d23cdc250057d1632968594a10f20edb870e8
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-04-14 23:15:42 +0300

    more tweaks

--- index.js
+++ index.js
@@ -13,145 +13,153 @@ document.addEventListener('DOMContentLoaded', () => {
   let loggedIn = false; 
 
   function fetchGames() {
-      fetch(apiUrl)
-          .then(response => {
-              if (!response.ok) {
-                  throw new Error('failure fetching games');
-              }
-              return response.json();
-          })
-          .then(data => {
-              displayGames(data);
-          })
-          .catch(error => {
-              console.error('error fetching games', error);
-          });
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+              throw new Error('failure fetching games');
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayGames(data);
+        })
+        .catch(error => {
+            console.error('error fetching games', error);
+        });
   }
   fetchGames();
 
   function displayGames(games) {
-      gameContainer.innerHTML = '';
-      games.forEach(game => {
-          const gameCard = createGameCard(game);
-          gameContainer.appendChild(gameCard);
-      });
+    gameContainer.innerHTML = '';
+    games.forEach(game => {
+        const gameCard = createGameCard(game);
+        gameContainer.appendChild(gameCard);
+    })
   };
 
   function createGameCard(game) {
-      const { title, thumbnail, genre, short_des } = game;
+    const { title, thumbnail, genre, short_des } = game;
 
-      const gameCard = document.createElement('div');
-      gameCard.classList.add('game-card');
+    const gameCard = document.createElement('div');
+    gameCard.classList.add('game-card');
 
-      const overlay = document.createElement('div');
-      overlay.classList.add('overlay');
-      gameCard.appendChild(overlay);
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    gameCard.appendChild(overlay);
 
-      const img = document.createElement('img');
-      img.src = thumbnail;
-      img.alt = title;
-      img.classList.add('game-thumbnail');
-      gameCard.appendChild(img);
+    const img = document.createElement('img');
+    img.src = thumbnail;
+    img.alt = title;
+    img.classList.add('game-thumbnail');
+    gameCard.appendChild(img);
 
-      const heading = document.createElement('h3');
-      heading.textContent = title;
-      gameCard.appendChild(heading);
+    const heading = document.createElement('h3');
+    heading.textContent = title;
+    gameCard.appendChild(heading);
 
-      const genreId = document.createElement('p');
-      genreId.textContent = genre;
-      gameCard.appendChild(genreId);
+    const genreId = document.createElement('p');
+    genreId.textContent = genre;
+    gameCard.appendChild(genreId);
 
-      const description = document.createElement('p');
-      description.textContent = short_des;
-      gameCard.appendChild(description);
+    const description = document.createElement('p');
+    description.textContent = short_des;
+    gameCard.appendChild(description);
 
-      const favouriteButton = document.createElement('button');
-      favouriteButton.innerHTML = '&#10084;';
-      favouriteButton.classList.add('favourite-button');
-      favouriteButton.addEventListener('click', function() {
-          if (loggedIn) {
-              console.log('favourite button clicked for game:', game['title']);
-              favouriteButton.style.color = favouriteButton.style.color === 'red' ? 'grey' : 'red';
-          } 
-          else {
-              alert('please login to like the game.');
+    const favouriteButton = document.createElement('button');
+    favouriteButton.innerHTML = '&#10084;';
+    favouriteButton.classList.add('favourite-button');
+    favouriteButton.addEventListener('click', function() {
+        if (loggedIn) {
+            console.log('favourite button clicked for game:', game['title']);
+            favouriteButton.style.color = favouriteButton.style.color === 'red' ? 'grey' : 'red';
+        } 
+        else {
+            alert('please login to like the game.');
           }
-      });
-      gameCard.appendChild(favouriteButton);
+    });
+    gameCard.appendChild(favouriteButton);
 
-      const commentButton = document.createElement('button');
-      commentButton.innerHTML = '&#128172;';
-      commentButton.classList.add('comment-button');
-      commentButton.addEventListener('click', function() {
-          if (loggedIn) {
-              openCommentModal(game);
-          } 
-          else {
-              alert('please login to comment on the game.');
-          }
-      });
-      gameCard.appendChild(commentButton);
+    const commentButton = document.createElement('button');
+    commentButton.innerHTML = '&#128172;';
+    commentButton.classList.add('comment-button');
+    commentButton.addEventListener('click', function() {
+        if (loggedIn) {
+            openCommentModal(game);
+        } 
+        else {
+            alert('please login to comment on the game.');
+        }
+    });
+    gameCard.appendChild(commentButton);
 
-      return gameCard;
+    return gameCard;
   }
 
   function openCommentModal(game) {
-      commentFormContainer.innerHTML = ''; // clear previous content
-      const commentForm = document.createElement('form');
-      const commentInput = document.createElement('textarea');
-      commentInput.setAttribute('placeholder', 'write your comment here...');
-      const commentSubmitButton = document.createElement('button');
-      commentSubmitButton.textContent = 'Submit';
-      commentSubmitButton.type = 'submit';
+    commentFormContainer.innerHTML = ''; // clear previous content
+    const commentForm = document.createElement('form');
+    const commentInput = document.createElement('textarea');
+    commentInput.setAttribute('placeholder', 'write your comment here...');
+    const commentSubmitButton = document.createElement('button');
+    commentSubmitButton.textContent = 'submit';
+    commentSubmitButton.type = 'submit';
 
-      commentForm.addEventListener('submit', function(event) {
-          event.preventDefault();
-          const commentText = commentInput.value.trim();
-          if (commentText !== '') {
-              // add code to submit the comment to the server
-              console.log(`comment submitted for game "${game.title}": ${commentText}`);
-              // display the comment on the page if needed
-          } 
-          else {
-              alert('please enter a comment before submitting.');
-          }
-      });
+    commentForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const commentText = commentInput.value.trim();
+        if (commentText !== '') { // add code to submit the comment to the server
+        console.log(`comment submitted for game "${game['title']}": ${commentText}`); // display the comment on the page if needed
+        } 
+        else {
+            alert('please enter a comment before submitting.');
+        }
+    });
 
-      commentForm.appendChild(commentInput);
-      commentForm.appendChild(commentSubmitButton);
-      commentFormContainer.appendChild(commentForm);
+    commentForm.appendChild(commentInput);
+    commentForm.appendChild(commentSubmitButton);
+    commentFormContainer.appendChild(commentForm);
 
-      loginModal.style.display = 'block'; // display the modal with comment form
+    loginModal.style.display = 'block'; // display the modal with comment form
   }
 
   favourite.addEventListener('click', () => {
-      console.log('favourite button clicked');
+    console.log('favourite button clicked');
   })
 
+  function toggleModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal.style.display === 'none' || modal.style.display === '') {
+        modal.style.display = 'block';
+        modal.style.zIndex = '1000'; // ensure the login modal is above other elements
+    } 
+    else {
+        modal.style.display = 'none';
+    }
+  }
   login.addEventListener('click', () => {
-      loginModal.style.display = 'block';
+    toggleModal('modal');
   });
 
   closeLoginModal.addEventListener('click', () => {
-      loginModal.style.display = 'none';
+    toggleModal('modal');
   });
 
-  window.addEventListener('click', (event) => {
-      if (event.target === loginModal) {
-          loginModal.style.display = 'none';
+  window.addEventListener('click', (e) => {
+      if (e.target === loginModal) {
+        toggleModal('modal');
       }
   });
 
   loginModalButton.addEventListener('click', () => {
       console.log('login button clicked');
       loggedIn = true; // set loggedIn to true when login button is clicked
-      loginModal.style.display = 'none'; // hide the login modal
+      toggleModal('modal'); // hide the login modal
   });
 
   signupModalButton.addEventListener('click', () => {
-      console.log('sign Up button clicked');
+      console.log('sign up button clicked');
       loggedIn = true; // set loggedIn to true when sign up button is clicked
-      loginModal.style.display = 'none'; // hide the login modal
+      toggleModal('modal'); // hide the login modal
   });
 
   login.addEventListener('click', () => {
@@ -159,50 +167,35 @@ document.addEventListener('DOMContentLoaded', () => {
     loginModal.style.zIndex = '1000'; // ensure the modal is above other elements
   });
 
-  closeLoginModal.addEventListener('click', () => {
-    loginModal.style.display = 'none';
-  });
-
-  window.addEventListener('click', (event) => {
-    if (event.target === loginModal) {
-        loginModal.style.display = 'none';
-    }
-  });
-
-  function toggleModal(modalId) {
-    const modal = document.getElementById(modalId);
-    if (modal.style.display === 'none' || modal.style.display === '') {
-        modal.style.display = 'block';
-        modal.style.zIndex = '1000'; // ensure the modal is above other elements
-    } 
-    else {
-        modal.style.display = 'none';
-    }
-  }
-
   function searchGames() {
-      const search = searchInput.value.toLowerCase();
-      fetch(apiUrl)
-          .then(response => {
-              if (!response.ok) {
-                  throw new Error('failure fetching games');
-              }
-              return response.json();
-          })
-          .then(data => {
-              const filteredGames = data.games.filter(game => {
-                  return game.title.toLowerCase().includes(search) || game.genre.toLowerCase().includes(search);
-              })
-              displayGames(filteredGames);
-          })
-          .catch(error => {
-              console.error('error fetching games', error);
-          });
+    const search = searchInput.value.toLowerCase().trim();
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('failure searching games');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const filteredGames = data.games.filter(game => {
+                return game.title.toLowerCase().includes(search) || game.genre.toLowerCase().includes(search);
+            });
+            if(filteredGames.length === 0) {
+                alert('game not found');
+            }
+            else {
+                displayGames(filteredGames);
+            }
+        })
+        .catch(error => {
+            console.error('error fetching games:', error);
+        });
   }
-
+  
   searchInput.addEventListener('keypress', function(e) {
-      if (e.key === 'enter') {
-          searchGames();
-      }
+    if (e.key === 'Enter') { // checks to see if enter is pressed
+        e.preventDefault(); // prevents form submission
+        searchGames();
+    }
   });
 });
